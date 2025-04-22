db10cea353b16aeab6e627138ef3d781fa18db22
walcruise <149022822+walcruise@users.noreply.github.com>
2025-04-22 22:04:04 +0100

    Add contract ABIs

---
+++ src/contracts/contractConfig.js
@@ -0,0 +1,10 @@
+export const contractConfig = {
+  longTrade: {
+    address: '0xe76aa7e39763e6fa260e13a24c6d76a8abf4305b',
+    functionName: 'initiateFlashLoan'
+  },
+  shortTrade: {
+    address: '0x92158730bee648250e0a10e44fef3661b8d2e2b8',
+    functionName: 'initiateFlashLoan'
+  }
+};
